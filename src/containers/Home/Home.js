6ba0f9f87a70c810e7dc2b96a09add898d1016f0
Kiyo Nishimura <kiyonish@gmail.com>
2017-07-18 21:05:53 +1000

    Update styles

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,16 +8,20 @@ import Helmet from 'react-helmet'
 
 export default class Home extends Component {
   componentDidMount = () => {
-    TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 1, { y: -20, delay: 2, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 1, { y: -10, delay: 2, ease: Expo.easeOut })
     TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 2, { delay: 2, css: { opacity: 0 }, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.copy), 4, { delay: 2.2, css: { opacity: 0 }, ease: Expo.easeOut })
   }
   render() {
     const styles = require('./Home.scss')
     return (
-      <div>
+      <div className={styles.home}>
         <Helmet title="Home"/>
-        <div ref="strapline" className={styles.home}>
-          I'm Kiyo, I'm a front end web develper based in Sydney, Australia.
+        <div ref="strapline" className={styles.strapline}>
+          I'm Kiyo, a freelance front end web developer based in Sydney, Australia.
+        </div>
+        <div ref="copy">
+          <p>For any project enquiries you can get in touch with me at kiyonish@gmail.com.</p>
         </div>
       </div>
     )
