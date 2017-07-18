79c4ef6ffa6c42295759e9597cbce7f7260fb297
Kiyo Nishimura <kiyonish@gmail.com>
2017-07-18 21:55:52 +1000

    Animation back on, SSR turned off to avoid flicker

--- src/components/navigation/SiteHeader.js
+++ src/components/navigation/SiteHeader.js
@@ -1,14 +1,14 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { TweenLite } from 'gsap'
-// import { Expo } from 'gsap/EasePack'
+import ReactDOM from 'react-dom'
+import { TweenLite } from 'gsap'
+import { Expo } from 'gsap/EasePack'
 import 'gsap/CSSPlugin'
 
 // import { Link } from 'react-router'
 export default class SiteHeader extends React.Component {
   componentDidMount = () => {
-    // TweenLite.from(ReactDOM.findDOMNode(this.refs.site_name), 1, { y: -20, delay: 1, ease: Expo.easeOut })
-    // TweenLite.from(ReactDOM.findDOMNode(this.refs.site_name), 2, { delay: 1, css: { opacity: 0 }, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.site_name), 1, { y: -20, delay: 1, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.site_name), 2, { delay: 1, css: { opacity: 0 }, ease: Expo.easeOut })
   }
 
   render() {
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-// import { TweenLite } from 'gsap'
-// import { Expo } from 'gsap/EasePack'
+import ReactDOM from 'react-dom'
+import { TweenLite } from 'gsap'
+import { Expo } from 'gsap/EasePack'
 import 'gsap/CSSPlugin'
 // import config from '../../config'
 import Helmet from 'react-helmet'
 
 export default class Home extends Component {
   componentDidMount = () => {
-    // TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 1, { y: -10, delay: 2, ease: Expo.easeOut })
-    // TweenLite.to(ReactDOM.findDOMNode(this.refs.strapline), 2, { delay: 2, css: { opacity: 1 }, ease: Expo.easeOut })
-    // TweenLite.to(ReactDOM.findDOMNode(this.refs.copy), 4, { delay: 2.2, css: { opacity: 0 }, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 1, { y: -10, delay: 2, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.strapline), 2, { delay: 2, css: { opacity: 0 }, ease: Expo.easeOut })
+    TweenLite.from(ReactDOM.findDOMNode(this.refs.copy), 4, { delay: 2.2, css: { opacity: 0 }, ease: Expo.easeOut })
   }
   render() {
     const styles = require('./Home.scss')
